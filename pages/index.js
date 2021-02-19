f7707e9ada9ca88bd74e3cd1e8d0e6f709ca9450
0x4bd0 <abdoumjr@gmail.com>
2021-02-19 19:58:56 +0100

    styled home screen

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Home page</h1>
-      <Link href="/projects">See Projects</Link>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+      <div className="btnCon"><Link href="/projects"><a>See Projects</a></Link></div>
     </div>
   )
 }
