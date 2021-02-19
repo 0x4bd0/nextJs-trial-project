885ccdeee35b93de88462f385249f065890c66b6
0x4bd0 <abdoumjr@gmail.com>
2021-02-19 21:58:52 +0100

    using getStaticProps to fetch data before ui render :)

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,14 +1,38 @@
 import Head from 'next/head'
 
-const Projects = () => {
-    return ( 
+export const getStaticProps = async() => {
 
+    const tmp = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let posts = await tmp.json()
+    if(posts.length > 0) {
+        posts = posts.splice(0,20)
+    } else {
+        posts = []
+    }
+
+    return {
+        props : {
+            posts
+        }
+    }
+}
+
+const Projects = ({posts}) => {
+    return ( 
         <>
         <Head>
         <title>Projects</title>
         </Head>
         <div>
             <h1>Projects :</h1>
+            {
+                posts.map(item=>(
+                    <div className="postsContianer" key={item.id}>
+                        <p className="postsTitle">{item.title}</p>
+                        <p className="postsBody">{item.body}</p>
+                    </div>
+                ))
+            }
         </div>
         </>
 
