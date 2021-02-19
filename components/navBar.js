ab51fedf8b973b4d3a7a062ee3be00062958f782
0x4bd0 <abdoumjr@gmail.com>
2021-02-19 19:28:22 +0100

    styling header

--- components/navBar.js
+++ components/navBar.js
@@ -2,16 +2,19 @@ import Link from 'next/link'
 
 const NavBar = () => {
     return ( 
-        <nLinkv>
-            <div clLinkssNLinkme="logo">
+        <nav>
+            <div className="logo">
                 <h1>Lorem Ipsum</h1>
             </div>
 
+
+            <div className="links">
             <Link href="/">Home</Link>
             <Link href="/about">Linkbout Us</Link>
             <Link href="/projects">Projects</Link>
-            
-        </nLinkv>
+            </div>
+
+        </nav>
      );
 }
  
